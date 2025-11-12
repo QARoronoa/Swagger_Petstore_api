1cd28322634dcaf86eb3833f8eff3e44ba90637e
issa Kamara <issa.kamara@ymail.com>
2025-11-12 17:09:50 +0100

    feat:ajout test add new pet

---
+++ test_add_a_new_pet.py
@@ -0,0 +1,36 @@
+import time
+from rich import print_json
+import allure
+import requests
+from utilities.ressources import *
+from utilities.configuration import config
+
+cfg = config()
+base = cfg['API']['base_url']
+url = f"{base}{apiRessources.addPet}"
+pet_id = int(time.time())
+
+payload = {
+    "id": pet_id,
+    "category": {"id": pet_id, "name": "UssopSama"},
+    "name": "UssopSama",
+    "photoUrls": ["string"],
+    "tags": [{"id": pet_id, "name": "string"}],
+    "status": "available"
+}
+
+def test_add_new_pet():
+    with allure.step('Appel POST /add a pet'):
+        response = requests.post(url, json=payload)
+
+        #allure attach
+        allure.attach(response.text)
+        allure.attach(str(response.status_code))
+
+        response_body = response.json()
+        print_json(data=response_body)
+
+        assert response.status_code == 200
+        assert response_body["id"] == pet_id
+        assert response_body["name"] == "UssopSama"
+        assert response_body["status"] == "available"
--- test_upload_image.py
+++ test_upload_image.py
@@ -7,7 +7,7 @@ from utilities.ressources import apiRessources
 cfg = config()
 base = cfg['API']["base_url"]
 
-pet_id = 198772
+pet_id = 1762962870
 url = f"{base}{apiRessources.postImage.format(petId=pet_id)}"
 path_image = r"C:\Users\Administrateur\Pictures\nav.PNG"
 
@@ -16,8 +16,9 @@ assert os.path.exists(path_image)
 
 #test
 def test_upload_une_image():
-    with open(path_image, 'rb') as f:
-        response = requests.post(url, files={'file':f}, timeout=10)
+    with allure.step('appel POST/uploadImage'):
+        with open(path_image, 'rb') as f:
+            response = requests.post(url, files={'file':f}, timeout=10)
 
 #allure attach
     allure.attach(str(response.status_code))
